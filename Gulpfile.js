67dc3335f590ee9bdb29f57d0b45dd949cea5e22
Igor Santana <santanna.ig@hotmail.com>
2024-07-22 16:38:13 -0300

    configuração inicial da hero section, criação de variaveis e utilização da metodologia BEM

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,28 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src('./source/styles/main.scss') // Certifique-se de que está apontando para o arquivo principal
+    return gulp.src('./source/styles/main.scss')
         .pipe(sass({
             outputStyle: 'compressed'
         }).on('error', sass.logError))
-        .pipe(gulp.dest('./build/styles/')); // Salva o arquivo compilado como main.css no diretório de destino
+        .pipe(gulp.dest('./build/styles/'));
+}
+
+function images() {
+    return gulp.src('./source/assets/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/assets/images'))
+        .on('error', function (err) {
+            console.error('Gulp Images Error:', err.message);
+        });
 }
 
 function watch() {
     gulp.watch('./source/styles/**/*.scss', styles);
+    gulp.watch('./source/assets/images/**/*', images);
 }
 
-exports.default = styles;
+exports.default = gulp.parallel(styles, images);
 exports.watch = watch;
