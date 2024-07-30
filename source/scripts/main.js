e8fc0d843f6cab0464cb29b53807ac1d1bfbd349
Igor Santana <santanna.ig@hotmail.com>
2024-07-30 16:01:54 -0300

    ajustes no hero

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -1,18 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
-    const heroSection = document.querySelector('.hero');
-    const alturaHero = heroSection.clientHeight;
-
-    window.addEventListener('scroll', function () {
-        const posicaoAtual = window.scrollY;
-
-        if(posicaoAtual <= alturaHero) {
-            ocultaElementosDoHeader();
-    }else{
-        exibeElementosDoHeader();
-    }
-});
 
     //Seção de atrações, programação das abas
     for (let i = 0; i < buttons.length; i++) {
@@ -33,15 +21,6 @@ document.addEventListener('DOMContentLoaded', function () {
         questions[i].addEventListener('click', abreOuFechaResposta);
     }
 });
-
-function ocultaElementosDoHeader(){
-    const header = document.querySelector('.header');
-    header.classList.add('header--is-hidden');
-}
-function exibeElementosDoHeader(){
-    const header = document.querySelector('.header');
-    header.classList.remove('header--is-hidden');
-}
 
 function abreOuFechaResposta(elemento) {
     const classe = 'faq__questions__item--is-open';
